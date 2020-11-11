0a894d30ac01636e88c3274c1981d45d8afa1091
Damiaou <damiaouu@gmail.com>
2020-11-11 13:07:08 +0100

    Add sort button

--- src/index.js
+++ src/index.js
@@ -120,6 +120,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
+          <button>Sort</button>
           <ol>{moves}</ol>
         </div>
       </div>
